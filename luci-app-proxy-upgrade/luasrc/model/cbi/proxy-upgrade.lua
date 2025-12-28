4503551a5e280a1af2f54be8bb306112e1fb8a85
zhengwuji <zhengwuji@users.noreply.github.com>
2025-12-28 11:26:13 +0800

    fix: 修复桥接模式下设备检测问题
    - 修改设备扫描逻辑,使用IP作为字典键而非MAC地址
    - 支持显示所有ARP表中的设备,无论是否在DHCP租约中
    - 完美支持桥接路由器和多IP设备
    - 更新README.md,添加新功能说明

--- luci-app-proxy-upgrade/luasrc/model/cbi/proxy-upgrade.lua
+++ luci-app-proxy-upgrade/luasrc/model/cbi/proxy-upgrade.lua
@@ -10,25 +10,44 @@ o.default = "0"
 
 o = s:option(Value, "proxy_ip", translate("代理服务器IP"))
 o.datatype = "ipaddr"
-o.rmempty = false 
+o.rmempty = false
 
--- Auto-discovery logic using luci.sys.net.host_hints()
-local sys = require "luci.sys"
-local hints = sys.net.host_hints()
-
-for mac, info in pairs(hints) do
-    local ip = info.ipaddr
-    local name = info.name or info.hostname
-    
-    if ip then
-        local label = ip
-        if name and name ~= "" then
-            label = label .. " (" .. name .. ")"
-        else
-            label = label .. " (" .. mac .. ")"
+-- Auto-discovery logic
+local f = io.open("/proc/net/arp", "r")
+local neighbors = {}
+if f then
+    f:read("*line") -- skip header
+    for line in f:lines() do
+        local ip, hw, flags, mac, mask, dev = line:match("(%S+)%s+(%S+)%s+(%S+)%s+(%S+)%s+(%S+)%s+(%S+)")
+        if ip and mac and dev == "br-lan" then
+            -- Use IP as key to avoid MAC address conflicts (same device with multiple IPs)
+            neighbors[ip] = {ip = ip, mac = mac}
         end
-        o:value(ip, label)
     end
+    f:close()
+end
+
+-- Try to get device names from DHCP leases (optional, for display only)
+f = io.open("/tmp/dhcp.leases", "r")
+if f then
+    for line in f:lines() do
+        local ts, mac, ip, name = line:match("(%S+)%s+(%S+)%s+(%S+)%s+(%S+)")
+        if ip and neighbors[ip] then
+            neighbors[ip].name = name
+        end
+    end
+    f:close()
+end
+
+-- Display all devices from ARP table
+for ip, info in pairs(neighbors) do
+    local label = info.ip
+    if info.name and info.name ~= "*" then
+        label = label .. " (" .. info.name .. ")"
+    else
+        label = label .. " (" .. info.mac .. ")"
+    end
+    o:value(info.ip, label)
 end
 
 o = s:option(Value, "proxy_port", translate("代理端口"))
@@ -38,10 +57,6 @@ o = s:option(ListValue, "proxy_type", translate("代理类型"))
 o:value("http", "HTTP")
 o:value("socks5", "SOCKS5")
 o.default = "http"
-
-o = s:option(Value, "timeout", translate("连接超时(秒)"))
-o.datatype = "uinteger"
-o.default = "5"
 
 o = s:option(Flag, "global_proxy", translate("出口UI设置"), translate("打勾代表，所有设备都会按照选择的IP，作为出口流量，也就是全局代理所有网路，使用的是选中的ip,确保网路软件配置不要冲突能正常使用，被选中的IP，不受影响"))
 o.default = "0"
